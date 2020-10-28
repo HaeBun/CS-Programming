c785909f9447b8894e5aab479755cbc29ea29c8b
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:36:17 +0900

    코드 2-6 : 정수와 연산자

---
+++ CS Programming/2장/ex02_06[IntegerBasic].cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_06_IntegerBasic_
+    {
+        // 코드 2-6 : 정수와 연산자
+        static void ex02_06(string[] args)
+        {
+            Console.WriteLine(1 + 2);
+            Console.WriteLine(1 - 2);
+            Console.WriteLine(1 * 2);
+            Console.WriteLine(1 / 2);
+            Console.WriteLine(1 % 2);
+        }
+    }
+}
