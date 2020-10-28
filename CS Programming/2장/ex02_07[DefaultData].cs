7977223dcb1beef1db5baf797066f8b2e489018c
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:38:10 +0900

    코드 2-7 : 음수와 나머지 연산자

---
+++ CS Programming/2장/ex02_07[DefaultData].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_07_DefaultData_
+    {
+        // 코드 2-7 : 음수와 나머지 연산자
+        static void ex02_07(string[] args)
+        {
+            Console.WriteLine(4 % 3);
+            Console.WriteLine(-4 % 3);
+            Console.WriteLine(4 % -3);
+            Console.WriteLine(-4 % -3);
+        }
+    }
+}
