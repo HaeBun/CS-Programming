a9ef6bfdd239b784eddcf130018cf5c6c3af3edd
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:42:59 +0900

    코드 2-9 : 정수와 실수

--- CS Programming/2장/ex02_09[DefaultData].cs
+++ CS Programming/2장/ex02_09[DefaultData].cs
@@ -9,7 +9,7 @@ namespace CS_Programming._2장
     class ex02_09_DefaultData_
     {
         // 코드 2-9 : 정수와 실수
-        static void Main(string[] args)
+        static void ex02_09(string[] args)
         {
             Console.WriteLine(0);
             Console.WriteLine(0.0);
