a2c52967813bc7d0e14523ceaa8616641568d184
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:39:08 +0900

    코드 2-8 : 실수

---
+++ CS Programming/2장/ex02_08[DefaultData].cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_08_DefaultData_
+    {
+        // 코드 2-8 : 실수
+        static void Main(string[] args)
+        {
+            Console.WriteLine(52.273);
+        }
+    }
+}
