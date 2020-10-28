681586c02b4d11acd887c2cfa92254ce1ad0a84c
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:31:52 +0900

    코드 2-4 : 연산자 우선순위

---
+++ CS Programming/2장/ex02_04[DefaultData].cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_04_DefaultData_
+    {
+        // 코드 2-4 : 연산자 우선순위
+        static void Main(string[] args)
+        {
+            // 결과를 예측해봅시다.
+            Console.WriteLine(5 + 3 * 2);
+        }
+    }
+}
