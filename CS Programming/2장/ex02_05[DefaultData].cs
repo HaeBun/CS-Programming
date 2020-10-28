98db2bdc6d41b53662799593a8c899505ff2a79a
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:34:02 +0900

    코드 2-5 : 나머지 연산자

---
+++ CS Programming/2장/ex02_05[DefaultData].cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_05_DefaultData_
+    {
+        // 코드 2-5 : 나머지 연산자
+        static void ex02_04(string[] args)
+        {
+            Console.WriteLine(10 % 5);
+            Console.WriteLine(7 % 3);
+        }
+    }
+}
