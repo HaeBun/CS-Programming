0b31bfbde4c6f7cc34b6c2e629a480129d06a739
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:26:06 +0900

    코드 1-2 : Hello World 출력

--- CS Programming/1장/ex01_02[FirstProgram].cs
+++ CS Programming/1장/ex01_02[FirstProgram].cs
@@ -8,6 +8,7 @@ namespace CS_Programming._1장
 {
     class FirstProgram
     {
+        // 코드 1-2 : Hello World 출력
         static void ex01_02(string[] args)
         {
             Console.WriteLine("Hello World..!");
