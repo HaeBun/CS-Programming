6661c5b12c7d05777537c6d6d28da526e37ac00f
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:55:09 +0900

    코드 2-12 : 문자

---
+++ CS Programming/2장/ex02_12[CharacterBasic].cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_12_CharacterBasic_
+    {
+        // 코드 2-12 : 문자
+        static void ex02_12(string[] args)
+        {
+            Console.WriteLine('A');
+            Console.WriteLine('가');
+            Console.WriteLine('나');
+        }
+    }
+}
