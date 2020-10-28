db243c946337c03706f1b4016e699c3413ce451f
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:41:03 +0900

    코드 2-9 : 정수와 실수

---
+++ CS Programming/2장/ex02_09[DefaultData].cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_09_DefaultData_
+    {
+        // 코드 2-9 : 정수와 실수
+        static void Main(string[] args)
+        {
+            Console.WriteLine(0);
+            Console.WriteLine(0.0);
+        }
+    }
+}
