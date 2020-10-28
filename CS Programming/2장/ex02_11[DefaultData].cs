f815eb46b5f9327f9a942f67be9141755e12a559
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:52:53 +0900

    코드 2-11 : 실수와 나머지 연산자

---
+++ CS Programming/2장/ex02_11[DefaultData].cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_11_DefaultData_
+    {
+        // 코드 2-11 : 실수와 나머지 연산자
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5.0 % 2.2);
+        }
+    }
+}
