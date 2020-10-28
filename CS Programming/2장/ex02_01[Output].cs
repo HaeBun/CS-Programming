9d421fd9fb21182fa720176c9688b848ee050f30
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:22:38 +0900

    코드 2-1 Hello World 예제

---
+++ CS Programming/2장/ex02_01[Output].cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_01_Output_
+    {
+        static void ex02_01(string[] args)
+        {
+            Console.WriteLine("Hello C# Programming .. !"); 
+        }
+    }
+}
