9a0f8372e364ad22c47afd0b6bb942dcf62aaa96
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:43:22 +0900

    코드 2-10 : 실수와 사칙 연산자

---
+++ CS Programming/2장/ex02_10[RealNumberBasic].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_10
+    {
+        // 코드 2-10 : 실수와 사칙 연산자
+        static void ex02_10(string[] args)
+        {
+            Console.WriteLine(1.0 + 2.0);
+            Console.WriteLine(1.0 - 2.0);
+            Console.WriteLine(1.0 * 2.0);
+            Console.WriteLine(1.0 / 2.0);
+        }
+    }
+}
