d206b210dc3cc9e33b8ae0a7108bdff37ad0f2ef
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:56:36 +0900

    코드 2-13 : 문자열

---
+++ CS Programming/2장/ex02_13[DefaultData].cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_13_DefaultData_
+    {
+        // 코드 2-13 : 문자열
+        static void ex02_13(string[] args)
+        {
+            Console.WriteLine("안녕하세요");
+        }
+    }
+}
