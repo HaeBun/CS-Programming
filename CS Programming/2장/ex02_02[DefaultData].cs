342efccbee5fd1fc449f91bd7cdd50c6e5bcfd4c
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:28:06 +0900

    코드 2-2 : 정수

--- CS Programming/2장/ex02_02[DefaultData].cs
+++ CS Programming/2장/ex02_02[DefaultData].cs
@@ -8,6 +8,7 @@ namespace CS_Programming._2장
 {
     class ex02_02_DefaultData_
     {
+        // 코드 2-2 : 정수
         static void ex02_02(string[] args)
         {
             Console.WriteLine(52);
