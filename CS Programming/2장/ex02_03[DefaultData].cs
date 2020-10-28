7fa39f5015be50bac9960b20143397862843cdec
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-10-28 20:30:32 +0900

    코드 2-3 : 정수 덧셈 연산자

---
+++ CS Programming/2장/ex02_03[DefaultData].cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_03_DefaultData_
+    {
+        // 코드 2-3 : 정수 덧셈 연산자
+        static void ex02_03(string[] args)
+        {
+            // 325를 출력합니다.
+            Console.WriteLine(52 + 273);
+        }
+    }
+}
