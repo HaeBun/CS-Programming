5af3a177d08e1e4973bdfd1880d78e076124a5be
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 17:25:55 +0900

    코드 2-46 : 증감 연산자 이해도 확인

---
+++ CS Programming/2장/ex02_46[Operators].cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_46_Operators_
+    {
+        // 코드 2-46 : 증감 연산자 이해도 확인
+        static void ex02_46(string[] args)
+        {
+            int number = 10;
+            Console.WriteLine(number++);
+            Console.WriteLine(++number);
+            Console.WriteLine(number--);
+            Console.WriteLine(--number);
+        }
+    }
+}
