d97a30afdc594c2f891cd64913a5c8427796128a
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 17:37:37 +0900

    코드 2-50 : var 키워드

---
+++ CS Programming/2장/ex02_50[VarKeyword].cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_50_VarKeyword_
+    {
+        // 코드 2-50 : var 키워드
+        static void ex02_50(string[] args)
+        {
+            var number = 100;
+        }
+    }
+}
