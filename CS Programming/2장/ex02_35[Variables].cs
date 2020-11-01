01a06e46637756433a9f6f0c8c07ce47d981f76e
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:52:06 +0900

    코드 2-35 : 문자 자료형과 연산자

---
+++ CS Programming/2장/ex02_35[Variables].cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_35_Variables_
+    {
+        // 코드 2-35 : 문자 자료형과 연산자
+        static void ex02_35(string[] args)
+        {
+            char a = 'a';
+            char b = 'b';
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+        }
+    }
+}
