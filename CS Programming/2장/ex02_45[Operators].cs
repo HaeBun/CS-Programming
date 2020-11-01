d299b9ae4320836fdf92f8ff01aa88318081b13c
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 17:24:01 +0900

    코드 2-45 : 후위 증감 증산자

---
+++ CS Programming/2장/ex02_45[Operators].cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_45_Operators_
+    {
+        // 코드 2-45 : 후위 증감 증산자
+        static void ex02_45(string[] args)
+        {
+            int number = 10;
+            Console.WriteLine(number);
+            Console.WriteLine(number); number += 1;
+            Console.WriteLine(number); number -= 1;
+            Console.WriteLine(number);
+        }
+    }
+}
