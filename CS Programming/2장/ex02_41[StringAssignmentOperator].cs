f3682b3bc551483a88c9549d59bf00e2821bf184
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 17:05:14 +0900

    코드 2-41 : 문자열과 관련된 복합 대입 연산자

---
+++ CS Programming/2장/ex02_41[StringAssignmentOperator].cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_41_StringAssignmentOperator_
+    {
+        // 코드 2-41 : 문자열과 관련된 복합 대입 연산자
+        static void Main(string[] args)
+        {
+            string output = "hello ";
+            output += "world ";
+            output += "!";
+
+            Console.WriteLine(output);
+        }
+    }
+}
