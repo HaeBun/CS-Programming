f163e4078f16fee4857f7b9c2c44d0d306972a69
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 15:58:02 +0900

    코드 2-18 : 문자 덧셈

---
+++ CS Programming/2장/ex02_18[DefaultData].cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_18_DefaultData_
+    {
+        // 코드 2-18 : 문자 덧셈
+        static void ex02_18(string[] args)
+        {
+            Console.WriteLine('가' + '힣');
+        }
+    }
+}
