aea83eddd706d2f9bf3097170f2702ad255ed74e
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:41:53 +0900

    코드 2-30 : long 자료형을 나타낸느 기호: 소문자

---
+++ CS Programming/2장/ex02_30[Variables].cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_30_Variables_
+    {
+        // 코드 2-30 : long 자료형을 나타낸느 기호: 소문자
+        static void ex02_30(string[] args)
+        {
+            Console.WriteLine(123456 + 654321);
+        }
+    }
+}
