7043eafb3c6bb7d47eaf518234d00d98cc7ad590
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:43:28 +0900

    코드 2-31 : long 자료형을 나타내는 기호 : 대문자

---
+++ CS Programming/2장/ex02_31[Variables].cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_31_Variables_
+    {
+        // 코드 2-31 : long 자료형을 나타내는 기호 : 대문자
+        static void Main(string[] args)
+        {
+            Console.WriteLine(123456 + 65432L);
+        }
+    }
+}
