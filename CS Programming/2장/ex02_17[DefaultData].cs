bee77de3d6986953fe7e0f0641e964418a9b601b
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 15:56:41 +0900

    코드 2-17 : 예외

---
+++ CS Programming/2장/ex02_17[DefaultData].cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_17_DefaultData_
+    {
+        // 코드 2-17 : 예외
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕하세요"[100]);
+        }
+    }
+}
