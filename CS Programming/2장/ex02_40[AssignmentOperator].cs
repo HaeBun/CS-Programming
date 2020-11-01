c2aa8fc2b52ee14a19ebc22a003786c169af5f04
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 17:03:32 +0900

    코드 2-40 : 숫자와 관련된 복합 대입 연산자 예제 풀어쓰기

---
+++ CS Programming/2장/ex02_40[AssignmentOperator].cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_40_AssignmentOperator_
+    {
+        // 코드 2-40 : 숫자와 관련된 복합 대입 연산자 예제 풀어쓰기
+        static void ex02_40(string[] args)
+        {
+            int output = 0;
+            output = output + 52;
+            output = output + 273;
+            output = output + 103;
+
+            Console.WriteLine(output);
+        }
+    }
+}
