5dd1a2760872c3bfc12116ffef3a831bfb22a807
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 19:09:44 +0900

    코드 2-54 : var 키워드를 사용한 다양한 자료형 선언

---
+++ CS Programming/2장/ex02_54[VarKeyword].cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_54_VarKeyword_
+    {
+        // 코드 2-54 : var 키워드를 사용한 다양한 자료형 선언
+        static void ex02_54(string[] args)
+        {
+            var numberA = 100L;  // long 자료형
+            var numberB = 100.0; // double 자료형
+            var numberC = 100.0F;// float 자료형
+        }
+    }
+}
