3ce6dc0610d64aef85f13525dc5ba576b3c798b0
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 17:21:35 +0900

    코드 2-44 : 증감 연산자의 전위와 후위

---
+++ CS Programming/2장/ex02_44[IncrementOperatorPosition].cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_44_IncrementOperatorPosition_
+    {
+        // 코드 2-44 : 증감 연산자의 전위와 후위
+        static void ex02_44(string[] args)
+        {
+            int number = 10;
+            Console.WriteLine(number);
+            Console.WriteLine(number++);
+            Console.WriteLine(number--);
+            Console.WriteLine(number);
+
+            number = 10;
+            Console.WriteLine(number);
+            Console.WriteLine(++number);
+            Console.WriteLine(--number);
+            Console.WriteLine(number);
+        }
+    }
+}
