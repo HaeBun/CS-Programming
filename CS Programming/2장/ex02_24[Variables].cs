1407ece3dbca1646b2d9471766e2592179c2b0a5
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:09:54 +0900

    코드 2-24 : 오버플로우

---
+++ CS Programming/2장/ex02_24[Variables].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_24_Variables_
+    {
+        // 코드 2-24 : 오버플로우
+        static void ex02_24(string[] args)
+        {
+            int a = 2147483640;
+            int b = 52273;
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
