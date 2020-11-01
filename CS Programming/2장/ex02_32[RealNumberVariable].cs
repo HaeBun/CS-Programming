6e93096b4623938727ceec997a370a033908f5a6
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:45:48 +0900

    코드 2-32 : 실수 변수 생성

---
+++ CS Programming/2장/ex02_32[RealNumberVariable].cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_32_RealNumberVariable_
+    {
+        // 코드 2-32 : 실수 변수 생성
+        static void Main(string[] args)
+        {
+            double a = 52.273;
+            double b = 103.32;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+        }
+    }
+}
