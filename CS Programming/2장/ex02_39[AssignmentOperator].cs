a09a8162105b125cec3028bbfb57a4f46f4fbdfe
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 17:01:39 +0900

    코드 2-39 : 숫자와 관련된 복합 대입 연산자

---
+++ CS Programming/2장/ex02_39[AssignmentOperator].cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_39_AssignmentOperator_
+    {
+        // 코드 2-39 : 숫자와 관련된 복합 대입 연산자
+        static void ex02_39(string[] args)
+        {
+            int output = 0;
+            output += 52;
+            output += 273;
+            output += 103;
+
+            Console.WriteLine(output);
+        }
+    }
+}
