75243e7bb8938debc006464d5563fe4d37512b8c
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:08:07 +0900

    코드 2-23 : 정수 변수 생성

---
+++ CS Programming/2장/ex02_23[IntegerVariable].cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_23_IntegerVariable_
+    {
+        // 코드 2-23 : 정수 변수 생성
+        static void ex02_23(string[] args)
+        {
+            int a = 273;
+            int b = 52;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+        }
+    }
+}
