25cd27fd71bfd342537b8d19e91137c432b62c3a
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 19:07:48 +0900

    코드 2-53 : var 키워드의 선언과 초기화 동시 수행

---
+++ CS Programming/2장/ex02_53[VarKeyword].cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_53_VarKeyword_
+    {
+        // 코드 2-53 : var 키워드의 선언과 초기화 동시 수행
+        static void ex02_53(string[] args)
+        {
+            var number = 20;
+        }
+    }
+}
