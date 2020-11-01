626b1cb3566c3d282973ab5584d72d4cc3c6e086
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 15:59:33 +0900

    코드 2-19 : 불

---
+++ CS Programming/2장/ex02_19[DefaultData].cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_19_DefaultData_
+    {
+        // 코드 2-19 : 불
+        static void ex02_19(string[] args)
+        {
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+        }
+    }
+}
