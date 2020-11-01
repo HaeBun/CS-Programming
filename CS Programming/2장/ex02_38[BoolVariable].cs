e7bddf5c5ef1cd4423ae2d191b5842d27791595c
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:57:11 +0900

    코드 2-38 : 불 변수 생성

---
+++ CS Programming/2장/ex02_38[BoolVariable].cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_38_BoolVariable_
+    {
+        // 코드 2-38 : 불 변수 생성
+        static void ex02_38(string[] args)
+        {
+            bool one = 10 < 0;
+            bool other = 20 > 100;
+
+            Console.WriteLine(one);
+            Console.WriteLine(other);
+        }
+    }
+}
