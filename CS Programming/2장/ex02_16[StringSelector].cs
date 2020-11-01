756e4af1d06c2461039e83b9a52f021e1dd638b8
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 15:55:32 +0900

    코드 2-16 : 문자 선택

---
+++ CS Programming/2장/ex02_16[StringSelector].cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_16_StringSelector_
+    {
+        // 코드 2-16 : 문자 선택
+        static void ex02_16(string[] args)
+        {
+            Console.WriteLine("안녕하세요"[0]);
+            Console.WriteLine("안녕하세요"[1]);
+            Console.WriteLine("안녕하세요"[3]);
+        }
+    }
+}
