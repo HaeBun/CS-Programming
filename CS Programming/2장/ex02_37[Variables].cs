95ef7e4505cd9b5f3faeba3c9e26265713bd1c55
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:55:48 +0900

    코드 2-37 : sizeof 연산자와 string 자료형

---
+++ CS Programming/2장/ex02_37[Variables].cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_37_Variables_
+    {
+        // 코드 2-37 : sizeof 연산자와 string 자료형
+        static void ex02_37(string[] args)
+        {
+            //Console.WriteLine("string: "+ sizeof(string));
+        }
+    }
+}
