4dbd2835666fd0f8d71d6ad1ced4ecd6a4397a97
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 17:33:31 +0900

    코드 2-48 : GetType() 메서드 활용

---
+++ CS Programming/2장/ex02_48[TypeCheck].cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_48_TypeCheck_
+    {
+        // 코드 2-48 : GetType() 메서드 활용
+        static void ex02_48(string[] args)
+        {
+            // 변수를 선언합니다.
+            int _int = 273;
+            long _long = 522731033265;
+            float _float = 52.273F;
+            double _double = 52.273;
+            char _char = '글';
+            string _string = "문자열";
+
+            // 출력합니다.
+            Console.WriteLine(_int.GetType());
+            Console.WriteLine(_long.GetType());
+            Console.WriteLine(_float.GetType());
+            Console.WriteLine(_double.GetType());
+            Console.WriteLine(_char.GetType());
+            Console.WriteLine(_string.GetType());
+        }
+    }
+}
