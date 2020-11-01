fe914fb4122f5de61ee09879f4b28464ffc69efd
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 19:12:04 +0900

    코드 2-55 문자열 입력과 출력

---
+++ CS Programming/2장/ex02_55[Input].cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_55_Input_
+    {
+        // 코드 2-55 문자열 입력과 출력
+        static void ex02_55(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine("input: " + input);
+        }
+    }
+}
