06c20d029493bbc2268b7241d995292441e1a3ec
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 15:52:01 +0900

    코드 2-14 : 이스케이프 문자

--- CS Programming/2장/ex02_14[EscapeCharacter].cs
+++ CS Programming/2장/ex02_14[EscapeCharacter].cs
@@ -9,7 +9,7 @@ namespace CS_Programming._2장
     class ex02_14_EscapeCharacter_
     {
         // 코드 2-14 : 이스케이프 문자
-        static void Main(string[] args)
+        static void ex02_14(string[] args)
         {
             Console.WriteLine("한빛\t아카데미");
             Console.WriteLine("한빛\n아카데미");
