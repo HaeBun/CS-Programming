5dc820a954dc2b1171b1e379e2057c2772641e78
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 17:38:54 +0900

    코드 2-51 : var 키워드의 제약

---
+++ CS Programming/2장/ex02_51[VarKeyword].cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_51_VarKeyword_
+    {
+        // 코드 2-51 : var 키워드의 제약
+        static void ex02_51(string[] args)
+        {
+            var number = 10.2;
+            //number = "변경";
+        }
+    }
+}
