fac24d3302c1e3b8fdc92cee17782c202e6637f5
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 17:09:49 +0900

    코드 2-43 : 증감 연산자

---
+++ CS Programming/2장/ex02_43[IncrementOperator].cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_43_IncrementOperator_
+    {
+        // 코드 2-43 : 증감 연산자
+        static void ex02_43(string[] args)
+        {
+            int number = 10;
+            number++;
+            Console.WriteLine(number);
+            number--;
+            Console.WriteLine(number);
+        }
+    }
+}
