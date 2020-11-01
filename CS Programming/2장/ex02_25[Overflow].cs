49394258d12451a6ad6444ec8c5ec5971089f940
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:11:50 +0900

    코드 2-25 : 오버플로우

---
+++ CS Programming/2장/ex02_25[Overflow].cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_25_Overflow_
+    {
+        // 코드 2-25 : 오버플로우
+        static void ex02_25(string[] args)
+        {
+            int a = 2000000000;
+            int b = 1000000000;
+            Console.WriteLine(a + b);
+        }
+    }
+}
