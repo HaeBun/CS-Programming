592982031e990a55db76a875c75fff541b52afb7
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:06:07 +0900

    코드 2-22 : 불과 논리 연산자

---
+++ CS Programming/2장/ex02_22[LogicalOperator].cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_22_LogicalOperator_
+    {
+        // 코드 2-22 : 불과 논리 연산자
+        static void ex02_22(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.Hour < 3 || 8 < DateTime.Now.Hour);
+            Console.WriteLine(3 < DateTime.Now.Hour && DateTime.Now.Hour < 8);
+        }
+    }
+}
