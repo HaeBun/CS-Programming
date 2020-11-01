7316df6852c4a8c5fc5ce8c1f9dd668eedf01aa8
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 17:07:53 +0900

    코드 2-42 : 문자열과 관련된 복합 대입 연산자 예제 풀어쓰기

--- CS Programming/2장/ex02_42[StringAssignmentOperator].cs
+++ CS Programming/2장/ex02_42[StringAssignmentOperator].cs
@@ -9,7 +9,7 @@ namespace CS_Programming._2장
     class ex02_42_StringAssignmentOperator_
     {
         // 코드 2-42 : 문자열과 관련된 복합 대입 연산자 예제 풀어쓰기
-        static void Main(string[] args)
+        static void ex02_42(string[] args)
         {
             string output = "hello ";
             output = output + "world ";
