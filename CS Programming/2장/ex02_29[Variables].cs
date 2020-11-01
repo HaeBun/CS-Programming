d5fd796ce910f0b60fbcf17c7f4357171fd4ba6c
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:40:26 +0900

    코드 2-29 : long 자료형의 최댓값과 최솟값

---
+++ CS Programming/2장/ex02_29[Variables].cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_29_Variables_
+    {
+        // 코드 2-29 : long 자료형의 최댓값과 최솟값
+        static void ex02_29(string[] args)
+        {
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+        }
+    }
+}
