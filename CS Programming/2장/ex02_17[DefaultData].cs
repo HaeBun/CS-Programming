d3bf4c2c0e625c9fc9c98bf3894b909e2109fe98
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 15:56:54 +0900

    코드 2-17 : 예외

--- CS Programming/2장/ex02_17[DefaultData].cs
+++ CS Programming/2장/ex02_17[DefaultData].cs
@@ -9,7 +9,7 @@ namespace CS_Programming._2장
     class ex02_17_DefaultData_
     {
         // 코드 2-17 : 예외
-        static void Main(string[] args)
+        static void ex02_17(string[] args)
         {
             Console.WriteLine("안녕하세요"[100]);
         }
