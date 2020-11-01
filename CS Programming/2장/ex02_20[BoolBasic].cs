1bcb96af1b6d1b9c43189d66f1e1882e4945d0e6
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:01:26 +0900

    코드 2-20 : 불과 비교 연산자

---
+++ CS Programming/2장/ex02_20[BoolBasic].cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_20_BoolBasic_
+    {
+        // 코드 2-20 : 불과 비교 연산자
+        static void ex02_20(string[] args)
+        {
+            Console.WriteLine(52 < 273);
+            Console.WriteLine(52 > 273);
+        }
+    }
+}
