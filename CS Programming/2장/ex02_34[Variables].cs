574bbb0ffa455d2555934e768365e297c354de3f
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:49:28 +0900

    코드 2-34 : sizeof 연산자

---
+++ CS Programming/2장/ex02_34[Variables].cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_34_Variables_
+    {
+        // 코드 2-34 : sizeof 연산자
+        static void ex02_34(string[] args)
+        {
+            Console.WriteLine("int: " + sizeof(int));
+            Console.WriteLine("long: " + sizeof(long));
+            Console.WriteLine("float: " + sizeof(float));
+            Console.WriteLine("double: " + sizeof(double));
+            Console.WriteLine("char: " + sizeof(char));
+        }
+    }
+}
