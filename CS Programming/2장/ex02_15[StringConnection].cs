f0a8e8bfc26f8a9cf5f9686ab7eb91d7b0032b4c
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 15:53:31 +0900

    // 코드 2-15 : 문자열 연결 연산자

---
+++ CS Programming/2장/ex02_15[StringConnection].cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_15_StringConnection_
+    {
+        // 코드 2-15 : 문자열 연결 연산자
+        static void ex02_15(string[] args)
+        {
+            Console.WriteLine("가나다" + "라마" + "바사아" + "자차카타" + "파하");
+        }
+    }
+}
