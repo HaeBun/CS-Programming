66453dc1ec841108f0e8bce813601cf845d0b450
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:47:42 +0900

    코드 2-33 : 문자 변수 생성

---
+++ CS Programming/2장/ex02_33[CharacterVariables].cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_33_CharacterVariables_
+    {
+        // 코드 2-33 : 문자 변수 생성
+        static void ex02_33(string[] args)
+        {
+            char a = 'a';
+            Console.WriteLine(a);
+        }
+    }
+}
