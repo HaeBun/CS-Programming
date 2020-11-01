3a84dfe9e19f572d14c49770b3f395e193598ea8
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:39:04 +0900

    코드 2-28 : int 자료형의 최댓값과 최솟값

---
+++ CS Programming/2장/ex02_28[Variables].cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    
+    class ex02_28_Variables_
+    {
+        // 코드 2-28 : int 자료형의 최댓값과 최솟값
+        static void ex02_28(string[] args)
+        {
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+        }
+    }
+}
