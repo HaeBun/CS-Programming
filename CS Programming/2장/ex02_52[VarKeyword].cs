91d8d263b444ceeabaa92edbeed251a006c7b3f5
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 17:40:42 +0900

    코드 2-52 : 지역변수

---
+++ CS Programming/2장/ex02_52[VarKeyword].cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_52_VarKeyword_
+    {
+        // 코드 2-52 : 지역변수
+        //var gloval = 52;
+
+        static void Main(string[] args)
+        {
+            var local = 273;
+        }
+    }
+}
