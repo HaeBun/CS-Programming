92e62f8f16208eccdabdd10d7ad84c04b7cbf652
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 17:28:00 +0900

    코드 2-47 : 여러 줄로 나누어 적은 증감 연산자

---
+++ CS Programming/2장/ex02_47[Operators].cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_47_Operators_
+    {
+        // 코드 2-47 : 여러 줄로 나누어 적은 증감 연산자
+        static void ex02_47(string[] args)
+        {
+            int number = 10;
+            Console.WriteLine(number);
+            number++;
+            number++;
+            Console.WriteLine(number);
+            Console.WriteLine(number);
+            number--;
+            number--;
+            Console.WriteLine(number);
+        }
+    }
+}
