e7045a145c638188ab54d7647f82a66be396f8f9
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-01 16:03:35 +0900

    코드 2-21 : 논리 부정 연산자

---
+++ CS Programming/2장/ex02_21[LogicalNot].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_21_LogicalNot_
+    {
+        // 코드 2-21 : 논리 부정 연산자
+        static void ex02_21(string[] args)
+        {
+            Console.WriteLine(!true);
+            Console.WriteLine(!false);
+            Console.WriteLine(!(52 < 273));
+            Console.WriteLine(!(52 > 273));
+        }
+    }
+}
