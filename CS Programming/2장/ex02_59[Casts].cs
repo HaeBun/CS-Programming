6c4c617f3a9373f846b7798dabecafe3a2355dd9
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 13:57:25 +0900

    코드 2-59 : 강제 자료형 변환의 데이터 손실

---
+++ CS Programming/2장/ex02_59[Casts].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_59_Casts_
+    {
+        // 코드 2-59 : 강제 자료형 변환의 데이터 손실
+        static void ex02_59(string[] args)
+        {
+            // long 자료형을 int 자료형으로 변환합니다.
+            long longNumber = 52273;
+            int intNumber = (int)longNumber;
+            Console.WriteLine(intNumber);
+        }
+    }
+}
