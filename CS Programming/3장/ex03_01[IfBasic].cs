2f85a5e2f98f819923751cc9951ec875ff773719
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 14:24:56 +0900

    코드 3-1 : 홀수 짝수 구분(1)

---
+++ CS Programming/3장/ex03_01[IfBasic].cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._3장
+{
+    class ex03_01_IfBasic_
+    {
+        // 코드 3-1 : 홀수 짝수 구분(1)
+        static void ex03_01(string[] args)
+        {
+            Console.Write("숫자 입력: ");
+            int input = int.Parse(Console.ReadLine());
+            
+            if(input % 2 == 0)
+            {
+                Console.WriteLine("짝수입니다!");
+            }
+
+            if(input % 2 == 1)
+            {
+                Console.WriteLine("홀수입니다!");
+            }
+        }
+    }
+}
