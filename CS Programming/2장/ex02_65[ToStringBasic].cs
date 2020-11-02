43e3c95e91ad456d2f0e597b0eb8e79365fd92c1
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 14:11:30 +0900

    코드 2-65 : 기본 자료형을 문자열로 변환

---
+++ CS Programming/2장/ex02_65[ToStringBasic].cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_65_ToStringBasic_
+    {
+        // 코드 2-65 : 기본 자료형을 문자열로 변환
+        static void ex02_65(string[] args)
+        {
+            Console.WriteLine((52).ToString());
+            Console.WriteLine((52.273).ToString());
+            Console.WriteLine(('a').ToString());
+            Console.WriteLine((true).ToString());
+            Console.WriteLine((false).ToString());
+
+            Console.WriteLine((52).ToString().GetType());
+            Console.WriteLine((52.273).ToString().GetType());
+            Console.WriteLine(('a').ToString().GetType());
+            Console.WriteLine((true).ToString().GetType());
+            Console.WriteLine((false).ToString().GetType());
+
+        }
+    }
+}
