bb943e9a74baaa658116e06a0ccee9dfbd6daf44
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 14:13:16 +0900

    코드 2-66 : 소수점 제거

---
+++ CS Programming/2장/ex02_66[DoubleToString].cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_66_DoubleToString_
+    {
+        // 코드 2-66 : 소수점 제거
+        static void ex02_66(string[] args)
+        {
+            double number = 52.273103;
+            Console.WriteLine(number.ToString("0.0"));
+            Console.WriteLine(number.ToString("0.00"));
+            Console.WriteLine(number.ToString("0.000"));
+            Console.WriteLine(number.ToString("0.0000"));
+        }
+    }
+}
