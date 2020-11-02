1b5550f275bef6ee6d98b815f8f7f7591c459495
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 14:20:24 +0900

    코드 2-69 : 문자열을 불로 변환

---
+++ CS Programming/2장/ex02_69[StringToBool].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_69_StringToBool_
+    {
+        // 코드 2-69 : 문자열을 불로 변환
+        static void ex02_69(string[] args)
+        {
+            Console.WriteLine(bool.Parse("True"));
+            Console.WriteLine(bool.Parse("true"));
+            Console.WriteLine(bool.Parse("False"));
+            Console.WriteLine(bool.Parse("false"));
+        }
+    }
+}
