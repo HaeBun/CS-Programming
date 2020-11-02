7e7dc34276dd1e8e55776861bec17bb31eab33ee
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 15:21:00 +0900

    코드 3-14 : 삼항 연산자

---
+++ CS Programming/3장/ex03_14[ConditionOperator].cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._3장
+{
+    class ex03_14_ConditionOperator_
+    {
+        // 코드 3-14 : 삼항 연산자
+        static void ex03_14(string[] args)
+        {
+            // 변수를 선언합니다
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            // 조건을 구분합니다
+            Console.WriteLine(number > 0 ? "자연수입니다" : "자연수가 아닙니다");
+        }
+        
+    }
+}
