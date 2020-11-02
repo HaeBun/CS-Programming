706cd4c1c4b60efdd4633186a38d1d4fae0b139e
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 14:02:20 +0900

    코드 2-61 : 자동 자료형 변환

---
+++ CS Programming/2장/ex02_61[ImplicitConversion].cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_61_ImplicitConversion_
+    {
+        // 코드 2-61 : 자동 자료형 변환
+        static void ex02_61(string[] args)
+        {
+            // int 자료형의 숫자를 생성합니다.
+            int intNumber = 2147483647;
+
+            // int 자료형을 long 자료형으로 자동 변환
+            long intToLong = intNumber;
+            Console.WriteLine(intToLong);
+
+            // int 자료형을 double 자료형으로 자동 변환
+            double intToDouble = intNumber;
+            Console.WriteLine(intToDouble);
+        }
+    }
+}
