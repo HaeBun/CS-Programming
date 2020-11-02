962b9ba20fb835b9ce275d8c69983295423f97d7
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 13:10:12 +0900

    코드 2-56 : 자료형 변환

---
+++ CS Programming/2장/ex02_56[Casts].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_56_Casts_
+    {
+        // 코드 2-56 : 자료형 변환
+        static void ex02_56(string[] args)
+        {
+            // long 자료형을 int 자료형으로 변환합니다.
+            long longNumber = 2147483647L + 2147483647L;
+            // int intNumber = longNumber;
+            // Console.WriteLine(intNumber);
+        }
+    }
+}
