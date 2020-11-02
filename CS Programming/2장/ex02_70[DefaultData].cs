a4d6a01ec9104c99cf2761e20aa8b06fe97f9b84
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 14:21:57 +0900

    코드 2-70 : int 자료형 최솟값의 음수

---
+++ CS Programming/2장/ex02_70[DefaultData].cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_70_DefaultData_
+    {
+        // 코드 2-70 : int 자료형 최솟값의 음수
+        static void ex02_70(string[] args)
+        {
+            int output = int.MinValue;
+            Console.WriteLine(-output);
+        }
+    }
+}
