e053189193549636a435d62e2ba4aff21db3776c
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 14:26:17 +0900

    코드 3-2 : 중괄호 사용 방식

---
+++ CS Programming/3장/ex03_02[Conditions].cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._3장
+{
+    class ex03_02_Conditions_
+    {
+        // 코드 3-2 : 중괄호 사용 방식
+        static void ex03_02(string[] args)
+        {
+            if (true)
+            {
+
+            }
+
+            if (true) { 
+
+            }
+        }
+    }
+}
