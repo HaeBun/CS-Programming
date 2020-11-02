862d15dd07cd3eae3d03f2924f20663d4afbf081
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 14:15:40 +0900

    코드 2-67 : 숫자와 문자열 덧셈

---
+++ CS Programming/2장/ex02_67[StringPlusNumber].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_67_StringPlusNumber_
+    {
+        // 코드 2-67 : 숫자와 문자열 덧셈
+        static void ex02_67(string[] args)
+        {
+            Console.WriteLine(52 + 273);
+            Console.WriteLine("52" + 273);
+            Console.WriteLine(52 + "273");
+            Console.WriteLine("52" + "273");
+        }
+    }
+}
