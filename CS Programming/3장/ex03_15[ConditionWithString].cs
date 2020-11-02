ba599a8733be730e3623966501e78f464a4256fd
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 15:23:19 +0900

    코드 3-15 : 입력 받아 조건 분할하기

---
+++ CS Programming/3장/ex03_15[ConditionWithString].cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._3장
+{
+    class ex03_15_ConditionWithString_
+    {
+        // 코드 3-15 : 입력 받아 조건 분할하기
+        static void ex03_15(string[] args)
+        {
+            Console.Write("입력 : ");
+            String line = Console.ReadLine();
+
+            if (line.Contains("안녕"))
+            {
+                Console.WriteLine("안녕하세요...!");
+            }
+            else
+            {
+                Console.WriteLine("^^");
+            }
+        }
+    }
+}
