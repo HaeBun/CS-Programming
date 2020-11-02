e2b318a51312f9eabe6cdb693ba94bf9e63511ae
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 15:29:18 +0900

    코드 3-16 : 키 입력 구분

---
+++ CS Programming/3장/ex03_16[ConditionWithKeyInput].cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._3장
+{
+    class ex03_16_ConditionWithKeyInput_
+    {
+        // 코드 3-16 : 키 입력 구분
+        static void ex03_16(string[] args)
+        {
+            ConsoleKeyInfo info = Console.ReadKey();
+            switch (info.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    Console.WriteLine("위로 이동");
+                    break;
+                case ConsoleKey.RightArrow:
+                    Console.WriteLine("오른쪽으로 이동");
+                    break;
+                case ConsoleKey.DownArrow:
+                    Console.WriteLine("아래로 이동");
+                    break;
+                case ConsoleKey.LeftArrow:
+                    Console.WriteLine("왼쪽으로 이동");
+                    break;
+                default:
+                    Console.WriteLine("다른 키를 눌렀습니다");
+                    break;
+            }
+        }
+    }
+}
