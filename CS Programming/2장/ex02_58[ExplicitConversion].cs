12252afa089bfbc41e82a1cabf6512a051776b64
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 13:50:15 +0900

    코드 2-58 : 강제 자료형 전환

---
+++ CS Programming/2장/ex02_58[ExplicitConversion].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_58_ExplicitConversion_
+    {
+        // 코드 2-58 : 강제 자료형 전환
+        static void ex02_58(string[] args)
+        {
+            // long 자료형을 int 자료형으로 변환합니다.
+            long longNumber = 2147483647L + 2147483647L;
+            int intNumber = (int)longNumber;
+            Console.WriteLine(intNumber);
+        }
+    }
+}
