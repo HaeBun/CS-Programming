a4a76d1471f6d92ba73ff6e6e8cec42185ed54fe
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 14:51:49 +0900

    코드 3-7 : 중첩 조건문 활용

---
+++ CS Programming/3장/ex03_07[NestedIfTIme].cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._3장
+{
+    class ex03_07_NestedIfTIme_
+    {
+        // 코드 3-7 : 중첩 조건문 활용
+        static void ex03_07(string[] args)
+        {
+            if (DateTime.Now.Hour < 11)
+            {
+                Console.WriteLine("아침 먹을 시간입니다.");
+            }
+            else
+            {
+                if(DateTime.Now.Hour < 15)
+                {
+                    Console.WriteLine("점심 먹을 시간입니다.");
+                }
+                else
+                {
+                    Console.WriteLine("저녁 먹을 시간입니다.");
+                }
+            }
+        }
+    }
+}
