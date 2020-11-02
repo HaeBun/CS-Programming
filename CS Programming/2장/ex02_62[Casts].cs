83072c25fa1d09b41e2f863ad87bae605c635232
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 14:04:14 +0900

    코드 2-62 : 문자열을 숫자로 변환

---
+++ CS Programming/2장/ex02_62[Casts].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_62_Casts_
+    {
+        // 코드 2-62 : 문자열을 숫자로 변환
+        static void ex02_62(string[] args)
+        {
+            // string 자료형을 int 자료형으로 변환합니다.
+            string numberString = "52273";
+            // int intNumber = (int)numberString;
+            // Console.WriteLine(intNumber);
+        }
+    }
+}
