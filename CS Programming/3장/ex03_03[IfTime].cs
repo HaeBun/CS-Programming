1f8822f0cb055dda7c255b0fbf689cde09cf3e19
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 14:28:20 +0900

    코드 3-3 : 현재 시간 구하기

---
+++ CS Programming/3장/ex03_03[IfTime].cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._3장
+{
+    class ex03_03_IfTime_
+    {
+        // 코드 3-3 : 현재 시간 구하기
+        static void ex03_03(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+        }
+    }
+}
