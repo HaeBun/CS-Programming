3f0872248c0bf02594804df9037057e86ab3725a
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 13:41:42 +0900

    코드 2-57 : 강제 자료형 전환

---
+++ CS Programming/2장/ex02_57[Casts].cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._2장
+{
+    class ex02_57_Casts_
+    {
+        // 코드 2-57 : 강제 자료형 전환
+        static void ex02_57(string[] args)
+        {
+            var a = (int)10.0;
+            var b = (float)10;
+            var c = (double)10;
+        }
+    }
+}
