19c64b87f744d994855c73e38faf2e103387dce4
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-02 14:43:05 +0900

    코드 3-6 : 오전과 오후 구분(2)

---
+++ CS Programming/3장/ex03_06[IfElseTime].cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._3장
+{
+    class ex03_06_IfElseTime_
+    {
+        // 코드 3-6 : 오전과 오후 구분(2)
+        static void ex03_06(string[] args)
+        {
+            if (DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine("오전입니다.");
+            }
+            else
+            {
+                Console.WriteLine("오후입니다.");
+            }
+        }
+    }
+}
