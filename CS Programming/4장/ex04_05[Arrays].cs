1d5948da4c75b625e88f2a3d5c84fb71786462d1
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-07 17:59:03 +0900

    코드 4-5 : 배열의 Length 속성

---
+++ CS Programming/4장/ex04_05[Arrays].cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_05_Arrays_
+    {
+        // 코드 4-5 : 배열의 Length 속성
+        static void ex04_05(string[] args)
+        {
+            // 배열을 생성합니다.
+            int[] intArray = { 52, 273, 32, 65, 103 };
+
+            // 배열의 길이를 출력합니다.
+            Console.WriteLine(intArray.Length);
+        }
+    }
+}
