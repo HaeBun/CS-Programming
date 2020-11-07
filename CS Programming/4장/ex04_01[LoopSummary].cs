bd2d9488f0c22fb990a43f81290bb8abfe07bc03
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-07 17:42:53 +0900

    코드 4-1 : 복사해서 붙여넣기를 사용한 반복

---
+++ CS Programming/4장/ex04_01[LoopSummary].cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_01_LoopSummary_
+    {
+        // 코드 4-1 : 복사해서 붙여넣기를 사용한 반복
+        static void ex04_01(string[] args)
+        {
+            Console.WriteLine("출력");
+            Console.WriteLine("출력");
+            Console.WriteLine("출력");
+            Console.WriteLine("출력");
+            Console.WriteLine("출력");
+        }
+    }
+}
