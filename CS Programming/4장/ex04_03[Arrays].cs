0c85a0b4738aa3b1c5e8f21f46509b5706f021ef
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-07 17:48:40 +0900

    코드 4-3 : 기본적인 배열 생성 방법

---
+++ CS Programming/4장/ex04_03[Arrays].cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_03_Arrays_
+    {
+        // 코드 4-3 : 기본적인 배열 생성 방법
+        static void ex04_03(string[] args)
+        {
+            int[] intArray = { 52, 273, 32, 65, 103 };
+            long[] longArray = { 52, 273, 32, 65, 103 };
+            float[] floatArray = { 1.0F, 2.0F, 3.0F, 4.0F, 5.0F };
+            double[] doubleArray = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+            char[] charArray = { '가', '나', '다', '라' };
+            string[] stringArray = { "윤인성", "연하진", "윤아린" };
+        }
+    }
+}
