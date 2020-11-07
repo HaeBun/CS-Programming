21b812a4d3f5097cb7c69f60f43a6db2c3bec09f
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-07 18:01:36 +0900

    코드 4-6 : 배열의 범위를 벗어나는 인덱스에 접근

---
+++ CS Programming/4장/ex04_06[Arrays].cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_06_Arrays_
+    {
+        // 코드 4-6 : 배열의 범위를 벗어나는 인덱스에 접근
+        static void ex04_06(string[] args)
+        {
+            // 배열을 생성합니다
+            int[] intArray = { 52, 273, 32, 65, 103 };
+
+            // 요소의 길이를 출력합니다.
+            Console.WriteLine(intArray[5]);
+        }
+    }
+}
