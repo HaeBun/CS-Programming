8a31f29cce25c5f73e9e718e0c628c189647e20a
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-07 18:10:41 +0900

    코드 4-7 : 원하는 크기의 배열 생성 방법

---
+++ CS Programming/4장/ex04_07[Arrays].cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_07_Arrays_
+    {
+        // 코드 4-7 : 원하는 크기의 배열 생성 방법
+        int[] array = new int[100];
+    }
+}
