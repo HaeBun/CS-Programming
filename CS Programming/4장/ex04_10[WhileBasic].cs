55a4997aa7174f7a106cf9e8890e201977adfa95
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-07 18:21:10 +0900

    코드 4-10 : while 반복문 이용하기

---
+++ CS Programming/4장/ex04_10[WhileBasic].cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_10_WhileBasic_
+    {
+        // 코드 4-10 : while 반복문 이용하기
+        static void ex04_10(string[] args)
+        {
+            // 변수를 선언합니다.
+            int i = 0;
+            int[] intArray = { 52, 273, 32, 65, 103 };
+            
+            // 반복을 수행합니다
+            while(i < intArray.Length)
+            {
+                // 출력합니다.
+                Console.WriteLine(i + "번째 출력:" + intArray[i]);
+
+                // 탈출을 위해 변수를 더합니다.
+                i++;
+            }
+        }
+    }
+}
