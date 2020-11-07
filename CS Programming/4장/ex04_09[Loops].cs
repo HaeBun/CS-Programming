c2237d6fdf614799fdc1da58b563e987b1486a23
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-07 18:18:29 +0900

    코드 4-9 : 무한 반복

---
+++ CS Programming/4장/ex04_09[Loops].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_09_Loops_
+    {
+        // 코드 4-9 : 무한 반복
+        static void ex04_09(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("무한 반복");
+            }
+        }
+    }
+}
