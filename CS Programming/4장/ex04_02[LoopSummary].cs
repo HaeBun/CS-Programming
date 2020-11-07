911e730482fa092dd37224fd0be943f09ce67969
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-07 17:45:05 +0900

    코드 4-2 : 반복문을 사용한 반복

---
+++ CS Programming/4장/ex04_02[LoopSummary].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_02_LoopSummary_
+    {
+        // 코드 4-2 : 반복문을 사용한 반복
+        static void ex04_02(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("출력");
+            }
+        }
+    }
+}
