cc3b73ea52c1c9babf8b25ec7bc965da8fbf6fd1
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-08 16:04:43 +0900

    코드 4-19 : for 코드 조각

---
+++ CS Programming/4장/ex04_19[DefaultData].cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_19_DefaultData_
+    {
+        // 코드 4-19 : for 코드 조각
+        static void ex04_19(string[] args)
+        {
+            int length = 0;
+            for(int i = 0; i < length; i++)
+            {
+
+            }
+        }
+    }
+}
