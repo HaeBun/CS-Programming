2d13d6afc89b4411ecd973ea93c4c515bf0992c4
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-08 16:06:14 +0900

    코드 4-30 : Console.SetCursorPosition() 메서드

---
+++ CS Programming/4장/ex04_30[MovingAt].cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_30_MovingAt_
+    {
+        // 코드 4-30 : Console.SetCursorPosition() 메서드
+        static void ex04_30(string[] args)
+        {
+            Console.Write("메서드 호출 전");
+            Console.SetCursorPosition(5, 5);
+            Console.Write("메서드 호출 후");
+        }
+    }
+}
