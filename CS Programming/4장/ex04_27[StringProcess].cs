b1b456384a51ac3b31730e0506a454a2fa6ccbb1
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-08 16:05:51 +0900

    코드 4-27 : 문자열 자르기

---
+++ CS Programming/4장/ex04_27[StringProcess].cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_27_StringProcess_
+    {
+        // 코드 4-27 : 문자열 자르기
+        static void ex04_27(string[] args)
+        {
+            string input = "감자 고구마 토마토";
+            string[] inputs = input.Split(new char[] { ' ' });
+
+            foreach (var item in inputs)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
