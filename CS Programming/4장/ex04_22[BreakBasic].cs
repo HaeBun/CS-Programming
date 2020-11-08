3edcc4bea944dafe0eb1d51b374c72ca46625d82
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-08 16:05:06 +0900

    코드 4-22 : break 키워드

---
+++ CS Programming/4장/ex04_22[BreakBasic].cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_22_BreakBasic_
+    {
+        // 코드 4-22 : break 키워드
+        static void ex04_22(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("숫자를 입력해주세요(짝수를 입력하면 종료): ");
+                int input = int.Parse(Console.ReadLine());
+                if (input % 2 == 0)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
