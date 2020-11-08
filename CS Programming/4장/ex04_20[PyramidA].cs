f0ed1fa55b32eb634ac83bf8c4438c64bbce98b8
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-08 16:04:51 +0900

    코드 4-20 : 별 피라미드(1)

---
+++ CS Programming/4장/ex04_20[PyramidA].cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_20_PyramidA_
+    {
+        // 코드 4-20 : 별 피라미드(1)
+        static void ex04_20(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                    Console.Write('*');
+                Console.Write('\n');
+            }
+        }
+    }
+}
