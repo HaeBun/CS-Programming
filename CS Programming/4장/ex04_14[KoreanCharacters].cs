13dc5af68e2d4e54278646e41ba6a2abe20ed9b4
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-08 16:04:00 +0900

    코드 4-14 : 한글 전부 출력하기

---
+++ CS Programming/4장/ex04_14[KoreanCharacters].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_14_KoreanCharacters_
+    {
+        // 코드 4-14 : 한글 전부 출력하기
+        static void ex04_14(string[] args)
+        {
+            for (int i = '가'; i <= '힣'; i++)
+            {
+                Console.WriteLine((char)i);
+            }
+        }
+    }
+}
