5734694a05ebea4b28bd74a103ab63f58f9e6f81
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-08 16:05:23 +0900

    코드 4-24 : continue 키워드

---
+++ CS Programming/4장/ex04_24[ContinueBasic].cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_24_ContinueBasic_
+    {
+        // 코드 4-24 : continue 키워드
+        static void ex04_24(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
