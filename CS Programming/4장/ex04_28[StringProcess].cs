b7f4a1bb579c9c28c7c17935d1fd256eb0012100
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-08 16:05:59 +0900

    코드 4-28 : 문자열 양 옆의 공백 제거

---
+++ CS Programming/4장/ex04_28[StringProcess].cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_28_StringProcess_
+    {
+        // 코드 4-28 : 문자열 양 옆의 공백 제거
+        static void ex04_28(string[] args)
+        {
+            string input = " test    \n";
+            Console.WriteLine("::" + input.Trim() + "::");
+            Console.Read();
+        }
+    }
+}
