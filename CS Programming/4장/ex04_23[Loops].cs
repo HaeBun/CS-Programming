6975df8edd8fb441e5fd67bef392dd5ea45cf17a
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-08 16:05:15 +0900

    코드 4-23 : goto 키워드

---
+++ CS Programming/4장/ex04_23[Loops].cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_23_Loops_
+    {
+        // 코드 4-23 : goto 키워드
+        static void ex04_23(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("외부 반복문");
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine("내부 반복문");
+                    goto doNotUse;
+                }
+            }
+        doNotUse:
+            Console.WriteLine("goto 키워드");
+        }
+    }
+}
