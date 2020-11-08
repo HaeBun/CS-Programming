87c3bc72236e3fd79fd1e29ff46e40ecec987901
kimit1279 <kimit1279@office.seoil.ac.kr>
2020-11-08 16:05:43 +0900

    코드 4-26 : 대문자화와 소문자화

---
+++ CS Programming/4장/ex04_26[StringProcess].cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Programming._4장
+{
+    class ex04_26_StringProcess_
+    {
+        // 코드 4-26 : 대문자화와 소문자화
+        static void ex04_26(string[] args)
+        {
+            string input = "Potato Tomato";
+            Console.WriteLine(input.ToUpper());
+            Console.WriteLine(input.ToLower());
+        }
+    }
+}
